ad2b7b72611d1c94d60506e8bb224100b032b99d
Vadim Frolov <fwadim@mail.ru>
2020-02-27 14:39:32 +0500

    maket

--- views/navbar.php
+++ views/navbar.php
@@ -3,9 +3,14 @@
 
 ?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+  <div class="navbar-title">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
-  </button>
+    </button>
+    <span class="video-title lwidth">
+      Video HD
+    </span>
+  </div>
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
@@ -91,9 +96,9 @@
       </li>
     </ul>
   </div>
-  <span class="video-title">
+  <span class="video-title hwidth">
     Video HD
-  </span>
+  </span>  
 </nav>
 
 <script type="text/javascript">
