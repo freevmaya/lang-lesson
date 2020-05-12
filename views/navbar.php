fb60f1cbf8b138503fbe546b533ff168b99a6774
Vadim Frolov <fwadim@mail.ru>
2020-05-12 08:31:35 +0500

    style

--- views/navbar.php
+++ views/navbar.php
@@ -31,7 +31,7 @@
 <?    
   }
 ?>
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
+<nav class="navbar navbar-expand-lg navbar-light">
   <div class="navbar-title">
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
